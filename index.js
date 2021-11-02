73f2eff7512adb28c907d8e752696525c03be43b
Бойчев Александр <bojcev_a@od.itstep.local>
2021-11-02 21:07:19 +0200

    2021.11.02.

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 //  Сервер на JS
 const HTTP_PORT = 8081;
-
+const WWW_ROOT = "www";
+const FILE_404 = WWW_ROOT + "/404.html";
 //Подключение модуля
 import { createServer } from "http";
+import fs from 'fs';
+//import fs from "fs"; // file system 
 
 // Серверная функция
 function serverFunction(request, response) {
-    //console.log(request);
+    //логируем запрос - это must have для всех нормальных серверов
     console.log(request.method + " " + request.url);
-    
-    if (request.url == '/home' || request.url == "/") {
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'text/html')
-        response.end("<h1>Home, sweet Home</h1>"); // ~getWriter().print
-    } else if(request.url == '/hello'){
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'text/html')
-        response.end("<h1>Hello, world</h1>");
-    }else if(request.url == '/js'){
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'text/html')
-        response.end("<h1>Node is Cool</h1>");
-    }else{
-        response.statusCode = 404;
-        response.setHeader('Content-Type', 'text/html')
-        response.end("<h1>Not Found</h1>");
+    //TODO: проверить запрос на спецсимволы (../)
+    // проверяем, является ли запрос файлом
+    const path = WWW_ROOT + request.url;
+    if (fs.existsSync(path) // объект существует
+    && fs.lstatSync(path).isFile()) {  // проверка является ли данный объект файлом
         
+        sendFile(path, response);
+        return;
+    }
+    //нет, эта не файл. Маршрутезируем
+    const url = request.url;
+    if (url == '/') {
+        // запрос / - передаём индексный файл
+        sendFile(WWW_ROOT + "/index.html", response);       
+    } else {
+        // необработанный запрос - "не найдено" (404.html)
+        sendFile(FILE_404, response, 404);  
     }
 
 }
@@ -42,3 +44,48 @@ server.listen(
         console.log("Listen start, port " + HTTP_PORT);
     }
 );
+
+//
+async function sendFile2(path, response, statusCode) {
+    fs.readFile( // открытие на чтение - асинхронно - отложенно
+        path, //путь к файлу
+        //sendFile// callback - выполнится после открытия файла
+        (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            if (typeof statusCode == 'undefined') {
+                statusCode = 200;
+            }
+            response.statusCode = statusCode;
+            response.setHeader('Content-Type', 'text/html; charset=utf-8');
+            response.end(data);
+        }
+    );
+}
+
+async function sendFile(path, response, statusCode=200) {
+    var readStream = false;
+    if (fs.existsSync(path)) {
+        readStream = fs.createReadStream(path);
+        //if (typeof statusCode == 'undefined') statusCode = 200;
+        response.statusCode = statusCode;
+    }else if (fs.existsSync(FILE_404)) {
+        readStream = fs.createReadStream(FILE_404);
+        statusCode = 404;
+    }
+    response.setHeader('Content-Type', 'text/html; charset=utf-8');
+    if (readStream) {
+        response.statusCode = statusCode;
+        readStream.pipe(response);
+    }else{
+        response.statusCode = 418;
+        response.end("I'm a teapot");
+    }
+    
+    // задание:  проверить наличие файла перед отправкой:
+    // 1. ищем файл, если есть - отправляем
+    // 2. если нет - ищем 404, отправляем (если есть)
+    // 3. если нет - отправляем строку с 418 кодом
+}
